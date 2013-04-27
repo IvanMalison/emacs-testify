e66ba0cd9dba146d7c71052da41dded5a6284a45
Ivan Malison <imalison@yelp.com>
2013-04-26 17:58:41 -0700

    Added support for setting sandbox from within emacs.

--- emacs-testify.el
+++ emacs-testify.el
@@ -1,5 +1,6 @@
 (defvar testify-project-file ".git" "The file that defines the project root")
 (defvar testify-project-root nil)
+(defvar testify-sandbox-directory nil)
 
 (defun testify-project-root ()
   "Return the root of the project."
@@ -19,16 +20,38 @@
 (defun testify-get-test-name ()
   (save-excursion
     (re-search-backward "def \\(.*?\\)(")
-    (message (match-string 1 nil))))
+    (match-string 1 nil)))
 
 (defun testify-get-case-name ()
   (save-excursion
     (re-search-backward "class \\(.*?\\)(")
-    (message (match-string 1 nil))))
+    (match-string 1 nil)))
 
 (defun testify-run (module &optional case_info)
   (compile (concat "cd " (testify-project-root)
-                   "; " "testify " module " " (or case_info ""))))
+                   "; " (testify-sandbox-command) "testify " module " " (or case_info ""))))
+
+(defun testify-get-id-from-pgconf-dir (directory-name)
+  (string-match "pgconf-\\(.*?\\)-\\(.*\\)/" directory-name)
+  `(,(match-string 2 directory-name) . ,directory-name))
+
+(defun testify-get-pgconf-names ()
+  (mapcar 'testify-get-id-from-pgconf-dir
+          (split-string (shell-command-to-string
+                         (concat "ls -a " (testify-project-root) " | grep .pgconf")))))
+
+(defun testify-sandbox-command ()
+  (if testify-sandbox-directory 
+      (concat "source " testify-sandbox-directory "/" "environment.sh; ") ""))
+
+(defun testify-set-sandbox ()
+  (interactive)
+  (let* ((sandbox-ids-to-files (testify-get-pgconf-names))
+         (sandbox-ids (delq nil (mapcar 'car sandbox-ids-to-files)))
+         (selection (if (and (boundp 'ido-mode) ido-mode)
+                        (ido-completing-read "Select a sandbox: " sandbox-ids)
+                      (completing-read "Find file in project: " sandbox-ids))))
+    (setq testify-sandbox-directory (cdr (assoc selection sandbox-ids-to-files)))))
 
 (defun testify-run-module ()
   (interactive)
@@ -44,3 +67,4 @@
                (concat (testify-get-case-name) "." (testify-get-test-name))))
 
 (provide 'emacs-testify)
+
